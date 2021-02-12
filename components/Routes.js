0a11051f7c1fa7aff05bf4d0f12bacc40dedcb8e
Isaac B. Conteh <67395857+Zacflixx@users.noreply.github.com>
2021-02-13 00:05:04 +0800

    added the delete function and page headers

--- components/Routes.js
+++ components/Routes.js
@@ -2,19 +2,34 @@ import Blogs from './Blogs';
 import Post from './Post';
 import Edit from './Edit';
 
+import StyleSheet from 'react-native';
+
 import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import {createAppContainer} from 'react-navigation';
 
-const NavStack = createStackNavigator({
-  Blogs: {
-    screen: Blogs,
+const NavStack = createStackNavigator(
+  {
+    Blogs: {
+      screen: Blogs,
+      navigationOptions: () => ({
+        headerTitle: 'Our Blogs',
+        // headerTintColor: 'red',
+      }),
+    },
+    Edit: {
+      screen: Edit,
+      navigationOptions: () => ({
+        headerTitle: 'Edit your text',
+        // headerTintColor: 'red',
+      }),
+    },
   },
-  Edit: {
-    screen: Edit,
+  {
+    headerLayoutPreset: 'center',
   },
-});
+);
 
 const BottomTab = createBottomTabNavigator({
   NavStack: {
